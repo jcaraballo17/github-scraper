4f5ff04872c85e12e2e9a024b2d287f228e00688
Juan <juan.caraballo17@gmail.com>
2021-02-07 18:42:33 -0400

    Removed static secret key from development settings file.

--- src/github_scraper/settings/development.py
+++ src/github_scraper/settings/development.py
@@ -1,5 +1,7 @@
+import secrets
+
 from github_scraper.settings.common import *
 
-SECRET_KEY: str = config.get('secret_key', '+t=7@g%yk4sa31t--qphl2gpf%8)plbd1+fezy8g^d3u)47x_+')
+SECRET_KEY: str = config.get('secret_key', secrets.token_urlsafe())
 
 DEBUG: bool = config.get('debug_mode', True)
